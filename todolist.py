34f8ecfa307f6e0eeb0feaa468997008c749b41f
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-07-22 13:15:07 +0530

    Taking a string entry

--- todolist.py
+++ todolist.py
@@ -3,5 +3,7 @@ from tkinter import *
 window=Tk()
 window.title("Todo List")
 content=Listbox(window,font="Ariel 24 bold")
+task=StringVar()
+e=Entry(window,textvariable=task,font="Ariel 24")
 
 window.mainloop()
