372be25e8e2982be56ef93946067428c36e53b96
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-07-22 13:25:27 +0530

    Making grid for content

--- todolist.py
+++ todolist.py
@@ -8,4 +8,6 @@ e=Entry(window,textvariable=task,font="Ariel 24")
 add=Button(window,text="ADD",font="Ariel 20",commond= lambda content=content,task=task :content.insert(END,task.get()))
 delete=Button(window,text="DELETE",font="Ariel 20",commond= lambda content=content : content.delete(ANCHOR))
 
+content.grid(row=0,column=0,columnspan=2,padx=5,pady=10)
+
 window.mainloop()
