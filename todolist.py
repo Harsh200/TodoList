daa4d36008b88052323760445a22e5e238b53a99
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-07-22 13:17:40 +0530

    adding and delete button

--- todolist.py
+++ todolist.py
@@ -5,5 +5,7 @@ window.title("Todo List")
 content=Listbox(window,font="Ariel 24 bold")
 task=StringVar()
 e=Entry(window,textvariable=task,font="Ariel 24")
+add=Button(window,text="ADD",font="Ariel 20",commond=)
+delete=Button(window,text="DELETE",font="Ariel 20",commond=)
 
 window.mainloop()
